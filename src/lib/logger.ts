20841d8c7e40aec9e838a249ec5fba8e1ed07dbc
nakshatra <combatrickshaw@gmail.com>
2024-02-19 23:21:28 +0530

    (monitoring): grafana loki setup for log aggregation

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,9 +1,19 @@
-import pino from "pino";
+import { createLogger, transports } from "winston";
+import LokiTransport from "winston-loki";
 
-const logger = pino({
-  transport: {
-    target: "pino-pretty",
-  },
-});
+function setupLogger() {
+  const opts = {
+    transports: [
+      new LokiTransport({
+        host: "http://192.168.137.221:3100"
+      })
+    ]
+  }
+
+  return createLogger(opts);
+}
+
+const logger = setupLogger();
 
 export default logger;
+
